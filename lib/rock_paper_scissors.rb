76758203f5adc1b83554afdd21c608c158b941da
Javier Jimenez and Sara Spink <javier@>
2017-09-06 10:12:52 -0700

    make the code more readable and update the tests and if else statements :

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -5,19 +5,15 @@ class RockPaperScissors
   end
 
   def beats(item)
-
-    if @item1 == "rock" && item == "scissors"
-      return true
-    # elsif item1 == "rock" && item2 == "paper"
-    #   return false
-    # elsif item1 == "scissors" && item2 == "paper"
-    #   return true
-    # elsif item1 == "scissors" && item2 == "rock"
-    #   return false
-    # elsif item1 == "paper" && item2 == "rock"
-    #   return true
-    # elsif item1 == "paper" && item2 == "scissors"
-    #   return false
+    if @item1 === "rock" && item === "scissors"
+      true
+    elsif @item1 === "scissors" && item === "paper"
+      true
+    elsif @item1 === "paper" && item === "rock"
+      true
+    else
+      false
     end
+    
   end
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -3,17 +3,19 @@ require('rock_paper_scissors')
 require('pry')
 
 rockEx = RockPaperScissors.new('rock')
+scissorsEx = RockPaperScissors.new('scissors')
+paperEx = RockPaperScissors.new('paper')
 
 describe ('#rock_paper_scissors') do
   it("returns true if rock is the object and scissors is the argument") do
     expect(rockEx.beats("scissors")).to(eq(true))
   end
 
-  # it("returns true if paper is the object and rock is the argument") do
-  #   expect("paper".beats?("rock")).to(eq(true))
-  # end
-  #
-  # it("returns true if scissors is the object and paper is the argument") do
-  #   expect("scissors".beats?("paper")).to(eq(true))
-  # end
+  it("returns true if scissors is the object and paper is the argument")do
+    expect(scissorsEx.beats("paper")).to(eq(true))
+  end
+
+  it("returns true if paper is the object and rock is the argument")do
+    expect(paperEx.beats("rock")).to(eq(true))
+  end
 end
