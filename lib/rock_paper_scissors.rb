5d0eeb898f8b474896c8afff39eef87b91a704e3
Javier Jimenez and Sara Spink <sara@>
2017-09-06 11:44:00 -0700

    more elsif statements and all passing specs and you can run the script

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,16 +4,43 @@ class RockPaperScissors
     @item1 = item1
   end
 
-  def beats(item)
+  def beats?(item)
     if @item1 === "rock" && item === "scissors"
-      true
+      puts "rock wins"
+      return true
+
+    elsif @item1 === "rock" && item === "paper"
+      puts "paper wins"
+      return false
+
     elsif @item1 === "scissors" && item === "paper"
-      true
+      puts "scissors wins"
+      return true
+
+    elsif @item1 === "scissors" && item === "rock"
+      puts "rock wins"
+      return false
+
     elsif @item1 === "paper" && item === "rock"
-      true
+      puts "paper wins"
+      return true
+
+    elsif @item1 === "paper" && item === "scissors"
+      puts "scissors wins"
+      return false
+
     else
-      false
+      puts "it's a tie"
+      return false
     end
-    
   end
+
+  puts "Player 1, please choose rock, paper, or scissors: "
+  input = gets.chomp
+  puts "Player 2 enter rock, paper or scissors: "
+  input2 = gets.chomp
+
+  inputEx = RockPaperScissors.new(input)
+  inputEx.beats?(input2)
+
 end
--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -8,14 +8,14 @@ paperEx = RockPaperScissors.new('paper')
 
 describe ('#rock_paper_scissors') do
   it("returns true if rock is the object and scissors is the argument") do
-    expect(rockEx.beats("scissors")).to(eq(true))
+    expect(rockEx.beats?("scissors")).to(eq(true))
   end
 
   it("returns true if scissors is the object and paper is the argument")do
-    expect(scissorsEx.beats("paper")).to(eq(true))
+    expect(scissorsEx.beats?("paper")).to(eq(true))
   end
 
   it("returns true if paper is the object and rock is the argument")do
-    expect(paperEx.beats("rock")).to(eq(true))
+    expect(paperEx.beats?("rock")).to(eq(true))
   end
 end
